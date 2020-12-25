d0cf42a4d828b5899b28c97e7df7001a7dda16ca
myke02 <learncodeenjoy@gmail.com>
2020-12-25 09:44:50 +0100

    flutter extract image's name from firebase_storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_iot_home/routes.dart';
 import 'package:my_iot_home/screens/dashboard_screen.dart';
@@ -12,13 +13,15 @@ import 'package:my_iot_home/services/push_notification_service.dart';
 //  ),
 //);
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
